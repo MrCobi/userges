ee33ccf3cc2971779de70e1aee7442847910de14
MrCobi <148904685+MrCobi@users.noreply.github.com>
2024-10-21 19:00:20 +0200

    userges

---
+++ MrCobi/userges/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { UsuarioAdminRoutedComponent } from './component/usuario/admin/plist/routed/usuario.admin.plist.routed.component/usuario.admin.plist.routed.component';
+
+
+export const routes: Routes = [
+  { path: 'admin/usuario/plist', component: UsuarioAdminRoutedComponent },
+];
